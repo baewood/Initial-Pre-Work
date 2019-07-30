b77ed80cd78ee4f8706209dc73c10252a707b87a
baewood <baewood@gmail.com>
2019-07-30 11:41:28 -0500

    Exercise 1 completed.

--- 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
+++ 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
@@ -1,3 +1,6 @@
-// Please remember to include a summary statement with EACH FILE regarding what you learned from the lesson in this format!  I recommend keeping it at the top of each file for faster processing of your application.
+// This exercise shows you how to make comments in your Javascript code.
 
-// Copy your code from the central panel of the freeCodeCamp page and paste it below after successfully passing the test.  You will have to click on the X at the top right of the modal that pops up to see the code. If you forget, just rerun the test.  You are also welcome to download your solution.
+// This is a comment
+
+/* This is also a comment
+but with multiple lines */
