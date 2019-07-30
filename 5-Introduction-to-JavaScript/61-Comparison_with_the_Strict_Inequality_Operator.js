763f784132e3b8c29dc93972b1b429dc425472c1
baewood <baewood@gmail.com>
2019-07-30 16:05:18 -0500

    Exercises 1-61 completed.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,18 @@
+// This lesson goes along with what was learned in previous lessons with comparison.
+// In this exercise, I learned about the strict inequality operator !==. It will not convert data types.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
