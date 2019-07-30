b481811ca39380a53ff054d3f3bbbcdd00fc09b6
baewood <baewood@gmail.com>
2019-07-30 16:02:43 -0500

    Exercises 1-60 completed.

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,13 @@
+// In this exercise, I learned about the inequality operator !=. It returns false where equality would return true.
+// It will convert data types of values while comparing.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(29);
