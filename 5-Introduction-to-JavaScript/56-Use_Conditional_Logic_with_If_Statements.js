21012f76673cb3b1eb9af96837fe475bdb604ca2
baewood <baewood@gmail.com>
2019-07-30 15:53:31 -0500

    Exercises 1-56 completed.

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,19 @@
+// In this lesson, I learned how to use conditional logic with if statements.
+// If statements are used to make decisions in code. The keyword tells JS to execute the code
+// in the curly braces under certain conditions. These are known as Boolean conditions: true or false.
+// If the condition is true, the program executes the if statement.
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
