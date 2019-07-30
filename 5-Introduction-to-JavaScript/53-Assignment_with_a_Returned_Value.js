283b42f5068cae9f120a82b67d5b5d3a44d21af5
baewood <baewood@gmail.com>
2019-07-30 15:40:52 -0500

    Exercises 1-53 completed.

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,11 @@
+// In this lesson, I learned how to take the return value of a function and assign it to a variable.
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
