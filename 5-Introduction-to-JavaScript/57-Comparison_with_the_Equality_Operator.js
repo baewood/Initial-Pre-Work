0df1833848bcfb9b230f8861eb04a1fd43cb2c3f
baewood <baewood@gmail.com>
2019-07-30 15:55:50 -0500

    Exercise 1-57 completed.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,13 @@
+// In this lesson, I learned that there are many comparison operators in JavaScript. They all return a Boolean
+// value. The most basic operator is the equality opterator ==, which we use in this exercise.
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
