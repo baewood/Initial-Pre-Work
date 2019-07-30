040f00da11002eb7687e0e8c6a0cb2397e9fbb7c
baewood <baewood@gmail.com>
2019-07-30 13:11:23 -0500

    Exercises 1-20 completed.

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,12 @@
+// This lesson continues with the same information learned in the previous exercises
+// except this lesson used the /= operator to divide a variable by another number.
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
