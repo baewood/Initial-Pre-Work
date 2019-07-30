76bec3724d861da99bbba252d201f55c7b217037
baewood <baewood@gmail.com>
2019-07-30 14:33:05 -0500

    exercises 1-36 completed.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,8 @@
+// In this exercise, I learned how to store multiple values in one variable using JS arrays.
+// I knew that arrays were in brackts with a comma between each entry.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["cats", 2];
