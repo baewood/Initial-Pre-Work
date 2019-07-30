38e573c3e13d913a8941cf9a48ada98124962493
baewood <baewood@gmail.com>
2019-07-30 14:03:00 -0500

    Exercises 1-29 completed.

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,16 @@
+// In this lesson, I learned how to find the length of a string using .length after the variable.
+// .length counts the number of character in the variable it is added to.
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
