54672221301ee91268626030e0fc11ac4c9c8365
baewood <baewood@gmail.com>
2019-07-30 11:50:09 -0500

    Exercises 1-3 completed.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,11 @@
+// In this lesson, I learned how to store values with an assinment operator.
+// I also learned that assignment goes from right to left, so everything to the right
+// of the operator is resolved before the value is assigned. 
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;
