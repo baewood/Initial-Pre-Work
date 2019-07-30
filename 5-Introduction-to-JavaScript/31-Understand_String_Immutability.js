0ba4aa2c77e2916ec6afbe81ed104fb0b9c47618
baewood <baewood@gmail.com>
2019-07-30 14:16:24 -0500

    Exercises 1-31 completed.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+// I took away that index and changed the assignment of the variable.
+// I learned immutable strings cannot be altered once created, and the only way to change it
+// is to assign a new string.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World";
