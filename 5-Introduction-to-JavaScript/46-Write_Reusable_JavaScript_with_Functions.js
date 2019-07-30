bc7fdcdf09fea678ae13678b8a300ef1295db4c6
baewood <baewood@gmail.com>
2019-07-30 15:02:03 -0500

    Exercises 1-46 completed.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,17 @@
+// I learned how to write reusable functions with JavaScript. Through previous coding experience,
+// I'm already familiar with how functions work. JS functions are written similarly to how C++ functions
+// are written. I learned that what's inside conole.log is what is seen, but only in the dev console.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
