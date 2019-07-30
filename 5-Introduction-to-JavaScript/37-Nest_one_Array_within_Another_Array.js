b1ed4079f85163fdb5104fab45a05b1a0d94a09b
baewood <baewood@gmail.com>
2019-07-30 14:34:41 -0500

    Exercises 1-37 completed.

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// I learned how to nest one array within another. I also learned that it is called a multi-dimensional array.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["cats", 2], ["dogs", 0]];
