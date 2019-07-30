c3a305eabff443c4e4d2d2b79f4b7c5573d3e90f
baewood <baewood@gmail.com>
2019-07-30 14:55:04 -0500

    Exercises 1-44 completed.

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,14 @@
+// I learned that unshift() works like push() but adds the element to the beginning of the array.
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul",35]);
