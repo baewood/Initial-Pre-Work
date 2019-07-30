d680a0df7446187c7342e40d5d86decfd0fada52
baewood <baewood@gmail.com>
2019-07-30 16:15:23 -0500

    Exercises 1-65 completed.

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,17 @@
+// In this lesson, I learned about the "less than or equal to" operator <=.
+// Like the previous comparison operators, it converts data types and returns Booleans.
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
