3cfef804532273741dd4742973ee01d3e4f9f529
baewood <baewood@gmail.com>
2019-07-30 14:47:50 -0500

    Exercises 1-41 completed.

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,14 @@
+// In this lesson, I learned about the push() function. .push() can take in one or more
+// parameters and push them to the end of the array without replacing what was already inside
+// the array.
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3])
