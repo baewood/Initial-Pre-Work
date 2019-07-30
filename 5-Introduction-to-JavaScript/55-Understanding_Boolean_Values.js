5e84400b6ead77fb3b97241f0d500df68483b5a9
baewood <baewood@gmail.com>
2019-07-30 15:50:21 -0500

    Exercises 1-55 completed.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,12 @@
+// This lesson goes over Boolean values which is one of two values: true or false.
+// The exercise explains it as true is "on" while false is "off."
+// I learned that Boolean values are never written quotes. They are not strings.
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
