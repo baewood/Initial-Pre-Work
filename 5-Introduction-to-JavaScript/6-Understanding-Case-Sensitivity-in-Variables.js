c73cabe6c1312e46dc51db22396df7a672143d08
baewood <baewood@gmail.com>
2019-07-30 12:16:04 -0500

    Exercises 1-6 completed.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,13 @@
+// This was a practice in understanding case sensitivity in variables.
+// I learned that declarations and assignments needed to match in case.
+// I also learned what camelCase was and how it was the proper way in JS to name variables. 
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
