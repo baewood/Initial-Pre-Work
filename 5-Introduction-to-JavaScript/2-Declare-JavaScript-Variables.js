f6fc25b2a42f95100917e70abe002715f7fe6706
baewood <baewood@gmail.com>
2019-07-30 11:45:09 -0500

    Exercises 1-2 comlpeted.

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,9 @@
+// In this lesson, I learned how to declare variables. To declare a variable in JS,
+// you need to put the keyword var in front of it. I also learned that variable names
+// can be made up of numbers, letters and $ or _, but can't contain spaces or start with a number.
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
