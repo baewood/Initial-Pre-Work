dad6d27c5195ff9bd7a9ff0e969f07710689a1d6
baewood <baewood@gmail.com>
2019-07-30 13:09:12 -0500

    Exercises 1-19 completed.

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,12 @@
+// In this lesson, I learned about the *= operator to multiply a variable by a number.
+// It goes along with the information learned in the previous exercises.
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
