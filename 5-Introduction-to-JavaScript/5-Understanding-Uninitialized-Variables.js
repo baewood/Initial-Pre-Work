ebbf2d267d7e5e1daf72f814d6cd76c0321e4d07
baewood <baewood@gmail.com>
2019-07-30 12:05:23 -0500

    Exercises 1-5 completed.

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,14 @@
+/* In this lesson, I learned that when JS variables are declared
+they have an initial value of undefined. Initializing variables avoids getting
+the string "undefined" and the result of NaN which means "not a number." */
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
