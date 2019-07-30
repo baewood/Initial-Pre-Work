38b920fea21117af3508b3c659cdd9ca52e1bf31
baewood <baewood@gmail.com>
2019-07-30 12:52:16 -0500

    Exercises 1-15 completed.

---
+++ 5-Introduction-to-JavaScript/15-Divide-One-Decimal-by-Another-with-JavaScript.js
@@ -0,0 +1,5 @@
+// In this lesson, I learned how to divide one decimal by another with JS.
+// It's essentially the same lesson as the one using whole numbers instead of floats.
+// The quotient variable equals out to 2.2.
+
+var quotient = 4.4 / 2.0; // Fixed this line
