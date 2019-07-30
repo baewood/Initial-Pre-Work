6e6b4c3cb86a72af4430b8c95d9db70e0ae36b10
baewood <baewood@gmail.com>
2019-07-30 13:00:26 -0500

    Exercises 1-17 completed.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,13 @@
+// In this lesson, I learned how to compound assignments with augmented addition.
+// To me, it's shorthand code. var a's outcome needed to be 15 so instead of writing
+// a = 3 + 12, var a is initialized and then compounded as a += 12 to equal 15. 
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
