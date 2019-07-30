5dce6f2462120738acb57744bcaed34e9cf0350f
baewood <baewood@gmail.com>
2019-07-30 13:45:49 -0500

    Exercises 1-25 completed.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// This lesson touched on concatenating strings, which I've become familiar with, by using the + operator.
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
