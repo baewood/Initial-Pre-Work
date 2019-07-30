a147126596516a0f647d9249e8f799404e1019f2
baewood <baewood@gmail.com>
2019-07-30 12:21:34 -0500

    Exercises 1-7 completed.

---
+++ 5-Introduction-to-JavaScript/7-Add-Two-Numbers-with-JavaScript.js
@@ -0,0 +1,5 @@
+// In this lesson, I learned how to add two numbers using JavaScript by using the +
+// symbol. The symbol is used as an addition operation when placed between two numbers.
+// This shows that the variable sum is assigned the number 20.
+
+var sum = 10 + 10;
