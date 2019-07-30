e5f26828f9d360baf1ed078262d5adc6f42489fd
baewood <baewood@gmail.com>
2019-07-30 13:43:39 -0500

    Exercises 1-24 completed.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,7 @@
+// In this lesson, I learned more about escape sequences in strings. I also learned that there
+// are two reasons to use escaping characters. One is to allow you to use character you might not be
+// able to type out, and the other is to allow you to represent multiple quotes in a string without JS
+// misinterpreting what you meant. In previous exercises, I learned about: \', \" and \\. From previous,
+// coding experience I knew about the newline character, but for me the following code was new: \r, \t, \b, and \f.
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
