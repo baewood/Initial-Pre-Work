0e097bc6c4f03f728dc54af2d58edb49e92096d5
baewood <baewood@gmail.com>
2019-07-30 13:24:59 -0500

    Exercises 1-22 completed.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,6 @@
+// I learned how to put double quotes inside of a string using double quotes.
+// I also learned that in JS, you can escape a quote from considering it as an end of a
+// string by using the backslash in front of the quote. It signals that it's not the end
+// of the string but that it should be inside of the string. 
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
