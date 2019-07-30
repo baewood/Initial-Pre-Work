7d9c10ea38c8ccdcbb31768f4b5a1189b360adaa
baewood <baewood@gmail.com>
2019-07-30 15:06:41 -0500

    Exercises 1-47 completed.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,16 @@
+// I learned how to pass values to functions with arguments. The exercise explains the difference
+// between parameters and arguments. Arguments are the values that are passed into the function when it's called.
+// The arguements in functionWithArgs were assigned to variables. 1 to a and 2 to b.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+
+functionWithArgs(1,2);
