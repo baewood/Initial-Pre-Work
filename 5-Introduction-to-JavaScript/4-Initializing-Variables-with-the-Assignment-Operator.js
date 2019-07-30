591d00f3c8fbc2a6c408e28b73aedb680857d85a
baewood <baewood@gmail.com>
2019-07-30 11:51:56 -0500

    Exercises 1-4 completed.

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,8 @@
+/* I learned that it's common to initialize a variable to an initial value in the same
+line that it is declared. */
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
