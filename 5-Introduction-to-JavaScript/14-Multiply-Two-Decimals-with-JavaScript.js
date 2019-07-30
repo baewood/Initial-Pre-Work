1494c5f3005c878b84601afca73f7adc1addc485
baewood <baewood@gmail.com>
2019-07-30 12:49:16 -0500

    Exercises 1-14 completed.

---
+++ 5-Introduction-to-JavaScript/14-Multiply-Two-Decimals-with-JavaScript.js
@@ -0,0 +1,4 @@
+//  This lesson is the same as a previous lesson that showed how to multiply with JS.
+// The difference is, in this lesson, floats are used. The variable "product" equals out to 5.
+
+var product = 2.0 * 2.5;
