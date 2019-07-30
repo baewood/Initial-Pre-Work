67f457c20879972f21bd1e78af23ac9afa7239fb
baewood <baewood@gmail.com>
2019-07-30 16:00:22 -0500

    Exercises 1-59 completed.

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,13 @@
+// This exercises is practive in comparing different values. It goes over what was learned in the previous two
+// exercises. The exercise was simple and I only had to change == to ===.
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
