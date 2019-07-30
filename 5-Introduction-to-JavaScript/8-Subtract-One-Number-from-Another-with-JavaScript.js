e6f9f2b5d41efb72cbb70d91819c1cf60143b554
baewood <baewood@gmail.com>
2019-07-30 12:23:08 -0500

    Exercises 1-8 completed.

---
+++ 5-Introduction-to-JavaScript/8-Subtract-One-Number-from-Another-with-JavaScript.js
@@ -0,0 +1,4 @@
+// This lesson is essentially the same as the previous, only using the - symbol for
+// subtraction. This shows that the variable difference is equal to 12.
+
+var difference = 45 - 33;
