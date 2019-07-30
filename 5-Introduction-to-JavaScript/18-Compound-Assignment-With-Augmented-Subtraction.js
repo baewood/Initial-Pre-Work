7a40d31df5d3739eccb95ca3e1926ac3f5c9c966
baewood <baewood@gmail.com>
2019-07-30 13:02:47 -0500

    Exercises 1-18 completed.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,12 @@
+// This is similar to the previous exercise only in this lesson to compound an assignment,
+// -= is used and subtracts a number from the variable.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
