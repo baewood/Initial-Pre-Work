c04eba7fb052f376cd3148367d3c2580e27bec1f
baewood <baewood@gmail.com>
2019-07-30 14:52:42 -0500

    Exercises 1-43 completed.

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,12 @@
+// I learned that .shift() removes the first element of an array, while .pop() removes the last.
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
