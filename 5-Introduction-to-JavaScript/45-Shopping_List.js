a0b94f431843976ec5614d822d20a1c62d3a61ef
baewood <baewood@gmail.com>
2019-07-30 14:58:41 -0500

    Exercises 1-45 completed.

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,6 @@
+// In this lesson, I had to create a shopping list in the given variable. I used what I learned
+// in previous exercises to add to the array. Instead of using multiple lines of code, I chose
+// to only use the push() function.
+
+var myList = [];
+myList.push(["Apples", 4], ["Bananas", 2], ["Beans", 3], ["Chocolates", 4], ["Milk",1]);
