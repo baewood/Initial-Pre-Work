108b00954c2b964731b6869a2d0059b41038dee5
baewood <baewood@gmail.com>
2019-07-30 14:50:55 -0500

    Exercises 1-42 completed.

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,13 @@
+// In this lesson, I learned what the .pop() function was. It "pops" the value off
+// of the end of an array and then returns the "popped off" element.
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
