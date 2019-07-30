71b4f3f91b6f402333e566ea3decfaf9f0b566b1
baewood <baewood@gmail.com>
2019-07-30 13:14:36 -0500

    Exercises 1-21 completed.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,11 @@
+// This lesson shows you how to declare string variables. It's called a string literal.
+// I learned it is a string because it is enclosed in single or double quotes.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Brittany";
+var myLastName = "Wood";
