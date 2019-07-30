2818dc170700ae342ac0ff9b51e4298bd5dcc083
baewood <baewood@gmail.com>
2019-07-30 13:57:16 -0500

    Exercises 1-27 completed.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,10 @@
+// I learned how to construct string with variables. It was a familiar concept so,
+// this exercise reaffirmed what I previously learned. 
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Brittany";
+var myStr = "Hello, my name is " + myName + ", what's up?";
