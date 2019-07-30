8c92b4c3e0e77150c1e23378d43625fa4f412b9f
baewood <baewood@gmail.com>
2019-07-30 15:22:46 -0500

    Exercises 1-51 completed.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,9 @@
+// In this lesson, I learned how to return a value from a function with return.
+// Once values are passed into a function with arguments, the return statement sends a
+// value back out of the function. The console.log checks the function and shows the answer in the dev console.
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
