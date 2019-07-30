80267e61c8c94c24737383a1e41778d0e0b478db
baewood <baewood@gmail.com>
2019-07-30 14:45:18 -0500

    Exercise 39 complete.

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,12 @@
+// In this lesson, I learned how to modify array data with indexes. Arrays are mutable, meaning
+// they can change. By assigning 45 to index 0 in myArray, it took the place of 18 which was previously in that index.
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
