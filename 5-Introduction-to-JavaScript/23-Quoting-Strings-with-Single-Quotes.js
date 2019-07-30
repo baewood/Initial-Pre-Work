60a192a3083d7f64a873f506548f3e1a27456a84
baewood <baewood@gmail.com>
2019-07-30 13:29:55 -0500

    Exercises 1-23 completed.

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,7 @@
+// This exercise had me removed the outer double quotes, and backslashes. I changed the
+// outer double quotes to single quotes. This helped removed the escape characters.
+// In this lesson, I learned the differences between what would be considered a good string and a
+// bad one in the lesson's examples. I didn't know that you could quote strings with single quotes,
+// I thought you could only use one type of quotes throughout the whole string. 
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
