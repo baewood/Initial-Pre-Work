a6edfabba059b550d115f61c68454f5eb753e436
baewood <baewood@gmail.com>
2019-07-30 13:48:01 -0500

    Exercises 1-26 completed.

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,11 @@
+// This exercise expands on the previous lesson. I learned how to concatenate using the += operator.
+// I also learned that it is important to watch out for spaces because concatenation doesn't add spaces between strings. 
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
