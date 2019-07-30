68ba3a2edb6f6b77bec785df59d2e82d1904187d
baewood <baewood@gmail.com>
2019-07-30 12:55:02 -0500

    Exercises 1-16 completed.

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,5 @@
+// In this lesson, I learned how to find a remainder in JavaScript using the remainder
+// operator %. I also learned that the remainder operator is sometimes referred to as the
+// "modulus" operator and that this is incorrect. It doesn't work properly with negative numbers.
+
+var remainder = 11 % 3; // The value is 2.
