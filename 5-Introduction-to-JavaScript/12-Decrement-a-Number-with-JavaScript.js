08a09d5117606243be708f288506c0a61ce74730
baewood <baewood@gmail.com>
2019-07-30 12:45:24 -0500

    Exercises 1-12 completed.

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,8 @@
+// The same method as incrimenting a number with JavaScript is essentially the same as
+// decrementing it, only this time we use the -- operator. So myVar = myVar - 1 becomes
+// myVar--.
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
