38afdd77038f3f1bce08a0bba5042ba2d4c70e4b
baewood <baewood@gmail.com>
2019-08-01 16:37:50 -0500

    Exercises 1-17 completed.

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,26 @@
+// I learned how to create strings using template literals. They are a special type of string that makes creating
+// complex strings easier. They allow you to create multi-line strings and to use string interpolation features
+// to create strings. I got the strings part from the commented out section of the exercise and used a template string
+// to pull from const result.
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = [`<li class="text-warning">${arr[0]}</li>`, `<li class="text-warning">${arr[1]}</li>` ,`<li class="text-warning">${arr[2]}</li>`]
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
