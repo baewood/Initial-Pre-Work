0c9870ff23c85d7baacd66670faa441b767dc2a7
baewood <baewood@gmail.com>
2019-08-01 13:59:34 -0500

    Exercises 1-89 completed.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,31 @@
+// This was a difficult challenge for me. It wouldn't run properly, at first. Until I noticed why it wouldn't run.
+// I learned that the comma went after the closed bracket not the second "LP" and was sad to see that was how the
+// example was written, and I still didn't notice. I learned that sometimes things just glaze over when coding.
+// The lesson was in manipulating complex objects and I learned that I should read everything while looking over the
+// instructions. I also learned about what a flexible Data Structure is.
+
+var myMusic = [
+ {
+   "artist": "Billy Joel",
+   "title": "Piano Man",
+   "release_year": 1973,
+   "formats": [
+     "CD",
+     "8T",
+     "LP"
+   ],
+   "gold": true
+ },
+ // Add record here
+ {
+  "artist": "Daft Punk",
+  "title": "Homework",
+  "release_year": 1997,
+  "formats": [
+    "CD",
+    "Cassette",
+    "LP"
+  ],
+   "gold": true
+ }
+];
