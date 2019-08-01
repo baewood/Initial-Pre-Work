7eac0443db4a0dcda1de9605d6f172e2a44e135c
baewood <baewood@gmail.com>
2019-08-01 12:59:32 -0500

    Exercises 1-77 completed.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,11 @@
+// In this lesson, I learned howt o return Boolean values from functions.
+// This exercises used comparison operators to change what was in the original if/else
+// statement into a return.
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
