52010e889bb598862eeefbab886c3552c2eec3c4
baewood <baewood@gmail.com>
2019-08-01 13:17:39 -0500

    Exercises 1-80 completed.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,22 @@
+// In this lesson, I learned how to build JavaScript objects. I also learned that objects are similar to
+// arrays, except it uses properties instead of indexes to access and modify data. 
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Baby",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Me and you!"]
+
+
+
+};
