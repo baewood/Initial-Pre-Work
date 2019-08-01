f1507cbae81670f52a6633d62636a0b2ec932365
baewood <baewood@gmail.com>
2019-08-01 13:20:58 -0500

    Exercises 1-81 completed.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,15 @@
+// In this lesson, I learned how to access object properties with dot notation.
+// I learned that dot notation is what is used when you know the name of the property you're
+// trying to access ahead of time. 
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Changed this line
+var shirtValue = testObj.shirt;    // Changed this line
