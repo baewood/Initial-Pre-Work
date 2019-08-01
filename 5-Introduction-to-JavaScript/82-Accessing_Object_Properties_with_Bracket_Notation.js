3667f40db992cad96a81c24c44b7c7b6eb5060c8
baewood <baewood@gmail.com>
2019-08-01 13:23:42 -0500

    Exercises 1-82 completed.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,15 @@
+// I learned that bracket notation and dot notation are similar when accessing object properties.
+// I also learned that if the property of the object has a space in its name, you need to use
+// bracket notation. Also, you can still use bracket notation if there are not spaces.
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Changed this line
+var drinkValue = testObj["the drink"];    // Changed this line
