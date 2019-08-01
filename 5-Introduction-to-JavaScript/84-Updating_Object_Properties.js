7cf3cf96e80891a4719b8d07c044f768c554d994
baewood <baewood@gmail.com>
2019-08-01 13:29:21 -0500

    Exercises 1-84 completed.

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,23 @@
+// I learned how to update object properties by using bracket and dot notation.
+// For this exercises, I used dot notation to update the property.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";
