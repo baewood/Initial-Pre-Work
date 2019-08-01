c554a9f0edcafc53b7e26307c1ae846e1c5c5bfa
baewood <baewood@gmail.com>
2019-08-01 15:45:43 -0500

    Exercises 1-7 completed.

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,9 @@
+// I learned how to write arrow functions with parameters. I also alearned that you can pass more than
+// one arguement into arrow functions. 
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
