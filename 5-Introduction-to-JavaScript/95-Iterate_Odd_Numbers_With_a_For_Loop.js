f744c407cfddbf6654c691585216549690e44a8e
baewood <baewood@gmail.com>
2019-08-01 14:39:02 -0500

    Exercises 1-95 completed.

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,17 @@
+// In this lesson, I learned how to iterate odd number with a for loop. Instead of using i++, I used i += whatever
+//number is needed to match what the exercise asks for.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2){
+  myArray.push(i);
+}
