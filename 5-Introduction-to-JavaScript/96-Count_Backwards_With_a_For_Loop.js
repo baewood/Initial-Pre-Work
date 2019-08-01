b73869170b513de0538b600032fe19c0932b18bc
baewood <baewood@gmail.com>
2019-08-01 14:40:39 -0500

    Exercises 1-96 completed.

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,16 @@
+// This exercises is similiar to the previous one, this time it counts backwards using i -= 2 in the final expression.
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
