5e25ac789f6359814e92038e5187088d5a988ee7
baewood <baewood@gmail.com>
2019-08-01 14:34:37 -0500

    Exercises 1-93 completed.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+// The JS while loop is like the C++ version. I reiterated what I've previously learned about loops to finish this
+// exercise.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+    myArray.push(i);
+    i++;
+}
