a7a5b17ce51998c09cc5da8f43302690efb87fad
baewood <baewood@gmail.com>
2019-08-01 14:54:08 -0500

    Exercises 1-99 completed.

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,13 @@
+// I learned about the do...while loop. It's used to run the same code multiple times. First will be "do" for one pass
+// and then it runs "while," the specified condition until it's no longer true.
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
