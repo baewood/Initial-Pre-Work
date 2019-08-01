850326e8b8e1ce715002b7a6962ceeed400d32ee
baewood <baewood@gmail.com>
2019-08-01 14:36:41 -0500

    Exercises 1-94 completed.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+// I learned that writing for loops has the same information as a while loop, but it is written on one line.
+// First is the initialization, then the condition and then the final expression.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for ( var i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
