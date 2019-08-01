5e614f0967c5eca53ca81bed558e30b8c8c72f07
baewood <baewood@gmail.com>
2019-08-01 15:31:22 -0500

    Exercises 1-3 completed.

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,17 @@
+// I learned about the const keyword and that variables declared using const are read-only.
+// Const means that they are a constant value, and cannot be reassigned.
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
