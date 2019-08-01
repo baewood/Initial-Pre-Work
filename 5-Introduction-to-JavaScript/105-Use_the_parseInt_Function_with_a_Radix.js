c4b4ecd0fb5f8e7c8500133be6932fced5790272
baewood <baewood@gmail.com>
2019-08-01 15:15:54 -0500

    Exercises 1-105 completed.

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,8 @@
+// I learned how to used the parseInt function with a Radix. I also learned that a Radix is
+// an integer between 2 and 36. In this exercise, I used 2.
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
