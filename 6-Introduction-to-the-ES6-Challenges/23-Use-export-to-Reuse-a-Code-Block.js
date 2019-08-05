647d458b0db147bea6253e2c99f35b4b5c440345
baewood <baewood@gmail.com>
2019-08-05 09:40:56 -0500

    Exercises 1-24 completed.

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,7 @@
+// In this exercise, I added export before the const variables. This lesson teaches how to use
+// export to reuse a code block. The examples were a little more complex than the exercise.
+// I learned that export is a non-browser feature. Export is used when you want to use that
+// file in another file, where you must import it.
+
+export const foo = "bar";
+export const bar = "foo";
---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,4 @@
+// I learned how to use * to import everything from a file using the import * as syntax.
+
+"use strict";
+import * as objectName from "capitalize_strings";
