dbfce3b2ee7f10c655e47ff4760e01a88ee3f992
baewood <baewood@gmail.com>
2019-08-05 09:29:40 -0500

    Exercises 1-22 completed.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+// I learned to understand the differences between import and require. I also learned that
+// require was a function in the past and that import was created to replace it.
+// There are a few ways to write an import statement but this exercises shows a common use-case.
+
+import {capitalizeString} from "string_functions";
+capitalizeString("hello!");
