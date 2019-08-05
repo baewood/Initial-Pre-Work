9741ba018f2fc5e431d882c09879b156a08935f2
baewood <baewood@gmail.com>
2019-08-05 09:49:31 -0500

    Intro to ES6 exercises completed.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,5 @@
+// In this exercise, I learned how to create an export fallback with export default.
+// I also learned this syntax is usually used if only one value is being exported from a file.
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,7 @@
+// I learned how to import a default export.  I also learned that the syntax is different
+// because curly braces aren't used. The primary method of importing a default export is to write
+// the value's name after import. 
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
